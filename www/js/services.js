b759658c60a0de37461a3a285e54af32cfd70d33
Javier Nieto <jnietom90@gmail.com>
2016-08-06 13:10:23 +0200

    first commit

---
+++ www/js/services.js
@@ -0,0 +1,67 @@
+angular.module('starter.services', [])
+
+.service('LoginService', function($http, ApiEndPoint) {
+    return {
+        loginUser: function(name, pw) {
+            var request = $http({
+                method: "POST",
+                url: ApiEndPoint.url + "/user/login/",
+                crossDomain:true,
+                data: {
+                    user: name,
+                    password: pw
+                }
+            });
+            return( request );
+        }
+    }
+})
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
